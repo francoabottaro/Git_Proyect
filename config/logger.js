552dd0eb18c2699773b2feb410e6988dcdbe76c5
Franco Bottaro <francoabottaro@gmail.com>
2024-09-16 17:46:44 -0300

    formated

--- config/logger.js
+++ config/logger.js
@@ -14,17 +14,17 @@ morgan.token('statusColor', (_req, res, _args) => {
 		status >= 500
 			? 31 // rojo
 			: status >= 400
-			? 33 // amarillo
-			: status >= 300
-			? 36 // cian
-			: status >= 200
-			? 32 // verde
-			: 0; // incoloro
+				? 33 // amarillo
+				: status >= 300
+					? 36 // cian
+					: status >= 200
+						? 32 // verde
+						: 0; // incoloro
 
 	return '\x1b[' + color + 'm' + status + '\x1b[0m';
 });
 module.exports = {
 	logger: morgan(
-		`:splitter\x1b[33m:method\x1b[0m \x1b[36m:url\x1b[0m :statusColor :response-time ms - length|:res[content-length]`
+		`:splitter\x1b[33m:method\x1b[0m \x1b[36m:url\x1b[0m :statusColor :response-time ms - length|:res[content-length]`,
 	),
 };
